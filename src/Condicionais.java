0a08ad98997c7e79c8366588db94229790b8422a
bill <cleber.flauzino@hotmail.com>
2023-01-18 23:54:28 -0300

    first commit

---
+++ src/Condicionais.java
@@ -0,0 +1,18 @@
+/**
+ * Condicionais
+ */
+public class Condicionais {
+
+  public static void main(String[] args) {
+
+    int time = 22;
+    if (time < 10) {
+      System.out.println("Good Morning");
+    }
+    if (time < 18) {
+      System.out.println("Good Day");
+    } else {
+      System.out.println("Good Everning");
+    }
+  }
+}
